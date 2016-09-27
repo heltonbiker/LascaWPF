b082dd0470340fdec8f43a7bf364ceca7e280769
heltonbiker <heltonbiker@gmail.com>
2016-09-27 00:04:57 -0300

    Primeiros testes já passando

---
+++ LascaDomain/Match.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LascaDomain
+{
+	public class Match
+	{
+		public Army Turn { get; private set; }
+
+		public IEnumerable<Move> AvailableMoves
+		{
+			get
+			{
+				var result = new List<Move>() { new Move(this) };
+
+				return result;
+			}
+		}
+
+		internal void ChangeTurn()
+		{
+			switch (Turn)
+			{
+				case Army.Black: Turn = Army.White; break;
+				case Army.White: Turn = Army.Black; break;
+			}
+		}
+
+		public static Match NewMatch()
+		{
+			Match board = new Match();
+			board.Turn = Army.White;
+			return board;
+		}
+
+		private void LoadStartPosition()
+		{
+			//throw new NotImplementedException();
+		}
+
+	}
+}
---
+++ LascaDomain/Move.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LascaDomain
+{
+	public class Move
+	{
+		private Match match;
+
+		public Move(Match match)
+		{
+			this.match = match;
+		}
+
+		public void Execute()
+		{
+			match.ChangeTurn();
+		}
+	}
+}
